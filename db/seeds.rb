2f9bfeece9af65058ca62f7335c7598b42bab9f2
gilsunshine <gilsunshine@gmail.com>
2018-03-01 08:38:39 -0500

    added bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-# Project.create(name: "Save the bears!", description: "Bears are dying. Adopt a bear today.", goal: 400)
-# Project.create(name: "Save the bees!", description: "Bees are dying. Adopt a bee today.", goal: 40003)
-# Project.create(name: "Save the whales!", description: "Whales are dying. Adopt a whale today.",goal: 2093)
+Project.create(name: "Save the bears!", description: "Bears are dying. Adopt a bear today.", goal: 400)
+Project.create(name: "Save the bees!", description: "Bees are dying. Adopt a bee today.", goal: 40003)
+Project.create(name: "Save the whales!", description: "Whales are dying. Adopt a whale today.",goal: 2093)
 
 Backer.create(name: "Gil", total_wallet: 5)
 Backer.create(name: "Arren", total_wallet: 10)
